a4715af9a507ccd42a31fc760fedec0cc4a18460
tomasjurasek <jurasek.tomas@hotmail.cz>
2016-11-18 15:45:40 +0100

    init

---
+++ ValidatableObject/ValidatableObject/Data.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidatableObject
+{
+    public class Data : IValidatableObject
+    {
+        public bool Enabled { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Enabled)
+            {
+                yield return new ValidationResult("Name is required! ", new[] { nameof(Name) });
+            }
+        }
+    }
+}
+
+
---
+++ ValidatableObject/ValidatableObject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidatableObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myData = new Data
+            {
+                Enabled = true
+            };
+
+            var results = new List<ValidationResult>();
+            Validator.TryValidateObject(myData, new ValidationContext(myData, null, null), results, true);
+
+            foreach (var result in results)
+            {
+                Console.WriteLine(result.ErrorMessage);
+            }
+        }
+    }
+}
